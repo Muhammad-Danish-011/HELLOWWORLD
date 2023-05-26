fdca5e805f4b9dd72a73613d6b86ea666b610cf3
Muhammad-Danish-011 <muhammad.danish@xloopdigital.com>
2023-05-26 09:16:45 -0500

    Initial commit

---
+++ src/main/java/com/example/atiaservice/AtiaserviceApplication.java
@@ -0,0 +1,14 @@
+package com.example.atiaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiaserviceApplication.class, args);
+		System.out.println("hellow world");
+	}
+
+}
